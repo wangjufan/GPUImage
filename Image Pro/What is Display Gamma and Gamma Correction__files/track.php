3632daff139b892ba747ac97fba85995b0282bf2
wangjufan <wangjufan@mail.51vv.com>
2020-05-25 21:39:30 +0800

    feat: vImage

---
+++ Image Pro/What is Display Gamma and Gamma Correction__files/track.php
@@ -0,0 +1,25 @@
+_mrd.cookie='ref_=mr_1;' + _mr_ex  + ';'+ 'path=/'+ ';'+ 'SameSite=Lax';var _mrTrackLinks = new Array;
+
+	                    _mrScanLinks();
+	                
+(function(){
+        var triggerCustomEvent = function( element, evName ) {
+            var ev, isIE = ( /*@cc_on!@*/false || !!document.documentMode );
+
+            if ( window.CustomEvent && !isIE ) {
+                ev = new CustomEvent( evName, {
+                    bubbles: true,
+                    cancelable: true
+                } );
+            } else {
+                ev = document.createEvent( 'Event' );
+                ev.initEvent( evName, true, true );
+            }
+
+            element.dispatchEvent( ev );
+        };
+
+        triggerCustomEvent( document.body, "opTrackingComplete" );
+        document.body.setAttribute( "data-op-tracked", true );
+         } )();
+
